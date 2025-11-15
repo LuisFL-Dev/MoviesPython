d0754cda286fed28d2704b561d44bc9a2d727ad0
LuisAcademy <luisf.barbosa@unifacol.edu.br>
2025-11-15 18:06:47 -0300

    atualizaçao

--- chatbot-filmes-main/core/config.py
+++
@@ -1,9 +0,0 @@
-# core/config.py
-from sqlalchemy import create_engine
-
-# Usando SQLite por padrão, mas pode ser trocado por PostgreSQL, MySQL, etc.
-# Ex: "postgresql://user:password@host:port/database"
-DATABASE_URL = "sqlite:///movies_database.db"
-
-# O Engine é o ponto central de comunicação com o banco de dados.
-engine = create_engine(DATABASE_URL)
--- chatbot-filmes-main/core/data/sor_movies.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS sor_movies (
-                                          id INTEGER,
-                                          title TEXT,
-                                          genres TEXT,
-                                          vote_average REAL,
-                                          vote_count INTEGER
-);
